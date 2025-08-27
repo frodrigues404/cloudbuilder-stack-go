0867382f4159ba0ee79c2f3b2590ec0094a25164
ferrodrigues <fernando.junior@corebiz.ag>
2025-08-27 00:04:02 -0300

    feat: rate limit

--- api_gateway.tf
+++ api_gateway.tf
@@ -8,7 +8,7 @@ module "api_gateway" {
   create_certificate    = false
   create_domain_name    = false
   create_domain_records = false
-
+  
   authorizers = {
     cognito = {
       authorizer_type  = "JWT"
@@ -21,5 +21,59 @@ module "api_gateway" {
     }
   }
 
-  routes = local.routes
+  stage_default_route_settings = {
+    throttling_rate_limit  = 10
+    throttling_burst_limit = 20
+  }
+
+  routes = {
+    "POST /register" = {
+      integration = {
+        uri                    = module.register_user_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      throttling_rate_limit  = 1
+      throttling_burst_limit = 3
+    }
+    "POST /login" = {
+      integration = {
+        uri                    = module.login_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      throttling_rate_limit  = 2
+      throttling_burst_limit = 4
+    }
+    "POST /verify-email" = {
+      integration = {
+        uri                    = module.confirm_email_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      throttling_rate_limit  = 1
+      throttling_burst_limit = 3
+    }
+    "DELETE /user" = {
+      integration = {
+        uri                    = module.delete_user_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      authorization_type = "JWT"
+      authorizer_key     = "cognito"
+    }
+    "POST /register-keys" = {
+      integration = {
+        uri                    = module.create_secret_keys_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      authorization_type = "JWT"
+      authorizer_key     = "cognito"
+    }
+    "POST /create-stack" = {
+      integration = {
+        uri                    = module.create_stack_lambda.lambda_function_arn
+        payload_format_version = "2.0"
+      }
+      authorization_type = "JWT"
+      authorizer_key     = "cognito"
+    }
+  }
 }
--- cloudformation-ms.tf
+++ cloudformation-ms.tf
@@ -1,31 +1,16 @@
 module "create_stack_lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "~> 8.0"
-
-  function_name                           = "${var.project}-create-stack-ms"
-  description                             = "Create CloudFormation Stack in target account"
-  handler                                 = "cmd/cloudformation-ms/create-stack/main.handler"
-  runtime                                 = "provided.al2023"
-  tracing_mode                            = "Active"
-  attach_policy_json                      = true
-  attach_tracing_policy                   = true
-  create_current_version_allowed_triggers = false
-  timeout                                 = 120
-  architectures                           = ["arm64"]
-
-  environment_variables = {
+  source             = "./modules/lambda"
+  name               = "${var.project}-create-stack-ms"
+  description        = "Create CloudFormation Stack in target account"
+  handler            = "${path.module}/cmd/cloudformation-ms/create-stack/main.handler"
+  path               = "${path.module}/cmd/cloudformation-ms/create-stack/cmd/lambda"
+  api_execution_arn  = module.api_gateway.api_execution_arn
+  attach_policy_json = true
+  variables = {
     USER_POOL_CLIENT_ID = aws_cognito_user_pool_client.client.id
     USER_POOL_ID        = aws_cognito_user_pool.user_pool.id
     REGION              = var.region
   }
-
-  allowed_triggers = {
-    APIGateway = {
-      service    = "apigateway"
-      source_arn = "${module.api_gateway.api_execution_arn}/*"
-    },
-  }
-
   policy_json = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -41,18 +26,4 @@ module "create_stack_lambda" {
       }
     ]
   })
-
-  source_path = [
-    {
-      path = "${path.module}/cmd/cloudformation-ms/create-stack/cmd/lambda"
-      commands = [
-        "GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o bootstrap main.go",
-        ":zip",
-      ]
-      patterns = [
-        "!.*",
-        "bootstrap",
-      ]
-    }
-  ]
 }
