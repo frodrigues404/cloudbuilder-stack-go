9884848ec63fde3ca9a8150f8e1e62b82202bf15
Fernando Rodrigues <fernandojr0227@gmail.com>
2025-10-06 20:33:17 -0300

    bucket

--- main.tf
+++ main.tf
@@ -38,6 +38,21 @@ module "cost_tracker_dynamodb" {
 
 }
 
+module "codepipeline_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.7.0"
+
+  bucket = "${var.project}-codepipeline-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = false
+  }
+}
+
 # module "cost_tracker_lambda" {
 #   source  = "terraform-aws-modules/lambda/aws"
 #   version = "~> 8.0"
